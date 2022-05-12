9ff3071f8bf9e93827317919259b022b0af0be8c
Luis Gabriel Modena <luisgabriel.modena326@gmail.com>
2022-05-12 16:27:13 -0300

    katas 1 realizado

--- katas1.js
+++ katas1.js
@@ -1,94 +1,168 @@
-function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
-}
+// function oneThroughTwenty() {
+//     let numeros = []
+//     for (let i = 1; i <= 20; i++) {
+//         numeros.push(i)
+//     }
+//     return numeros
+// }
 
-//call function oneThroughTwenty
+// console.log(oneThroughTwenty())
 
-function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
-}
+// call
+// function oneThroughTwenty
 
-//call function evensToTwenty
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
-}
+// function evensToTwenty() {
+//     let par = []
+//     for (let i = 1; i <= 20; i++) {
+//         if (i % 2 === 0) {
+//             par.push(i);
+//         }
+//     }
 
-//call function oddsToTwenty
+//     return par;
 
-function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+// }
 
-}
+// console.log(evensToTwenty())
+// call
+// function evensToTwenty
 
-//call function multiplesOfFive
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
-}
+// function oddsToTwenty() {
+//     let impar = []
+//     for (let i = 1; i <= 20; i++) {
+//         if (i % 2 === 1) {
+//             impar.push(i);
+//         }
+//     }
+//     return impar;
 
-//call function squareNumbers
+// }
 
-function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
+// console.log(oddsToTwenty())
 
-//call function countingBackwards
+// call
+// function oddsToTwenty
 
-function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-//call function evenNumbersBackwards
+// function multiplesOfFive() {
+//     let multiplosCinco = []
+//     for (let i = 1; i <= 100; i++) {
+//         if (i % 5 === 0) {
+//             multiplosCinco.push(i)
+//         }
+//     }
+//     return multiplosCinco;
 
-function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
+// }
 
-//call function oddNumbersBackwards
 
-function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
+// console.log(multiplesOfFive())
+// call
+// function multiplesOfFive
 
-//call function multiplesOfFiveBackwards
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
+// function squareNumbers() {
+//     let quadrados = [];
 
-//call function squareNumbersBackwards
+//     for (let i = 1; i <= 100; i++) {
+//         if (Math.sqrt(i) % 1 === 0) {
+//             quadrados.push(i)
+//         }
+//     }
+//     return quadrados
+// }
+// console.log(squareNumbers());
+
+// call
+// function squareNumbers
+
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+// function countingBackwards() {
+//     let numerosTras = [];
+
+//     for (let i = 20; i >= 1; i--) {
+//         numerosTras.push(i)
+//     }
+//     return numerosTras
+// }
+// console.log(countingBackwards())
+
+// call
+// function countingBackwards
+
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+// function evenNumbersBackwards() {
+//     let numerosTrasPar = [];
+
+//     for (let i = 20; i >= 1; i--) {
+//         if (i % 2 === 0) {
+//             numerosTrasPar.push(i)
+//         }
+//     }
+//     return numerosTrasPar
+// }
+// console.log(evenNumbersBackwards())
+// call
+// function evenNumbersBackwards
+
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+// function oddNumbersBackwards() {
+//     let numerosTrasImpar = [];
+
+//     for (let i = 20; i >= 1; i--) {
+//         if (i % 2 == 1) {
+//             numerosTrasImpar.push(i)
+//         }
+//     }
+//     return numerosTrasImpar
+// }
+// console.log(oddNumbersBackwards())
+
+// call
+// function oddNumbersBackwards
+
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+// function multiplesOfFiveBackwards() {
+//     let multiplosCincoTras = [];
+
+//     for (let i = 20; i >= 1; i--) {
+//         if (i % 5 == 0) {
+//             multiplosCincoTras.push(i)
+//         }
+//     }
+//     return multiplosCincoTras
+// }
+
+// console.log(multiplesOfFiveBackwards())
+
+// call
+// function multiplesOfFiveBackwards
+
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+// function squareNumbersBackwards() {
+//     let quadradosTras = [];
+
+//     for (let i = 100; i >= 1; i--) {
+//         if (Math.sqrt(i) % 1 === 0) {
+//             quadradosTras.push(i)
+//         }
+//     }
+//     return quadradosTras
+// }
+
+// console.log(squareNumbersBackwards())
+
+
+// call
+// function squareNumbersBackwards
